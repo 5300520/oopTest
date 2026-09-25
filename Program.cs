12942b15e3d2fd0ccea50dd6fac6befb40268e83
comp5300520 <comp5300520@Q044442>
2026-09-25 12:15:11 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+namespace oopTest
+{
+    internal class Program
+    {
+        class Car
+        {
+            public string Colour { get; } = "red";
+            /*makes a class called Car with a variable 
+             */
+        }
+        static void Main(string[] args)
+        {
+            Car myObj = new Car();
+            WriteLine(myObj.Colour);
+        }
+    }
+}
